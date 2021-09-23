a8efb4d2f3f565ed81877b626b1042e77ec6a98c
dontechman <isbncd89@gmail.com>
2021-09-24 02:51:37 +0900

    환경셋팅 완료

--- src/main/java/com/cos1/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos1/blog/test/BlogControllerTest.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BlogControllerTest {
+public class BlogControllerTest {  
 
 	@GetMapping("/test/hello")
 	public String hello() {
